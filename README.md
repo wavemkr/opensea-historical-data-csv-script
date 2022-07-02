e3bd9316e7536fbcd82e060bb6e1b5cbf28bad12
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2022-07-01 23:26:35 -0600

    Cooler stuff

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 ## Setup
 
 - Requires [Node](https://nodejs.org/en/download/) installed. Minimum version v16.13.0
-- Requires an Opensea API key. To add your key, copy the file `.env.template` to just `.env` and replace the "12345" with your API key.
 - Requires dependencies to be installed. Run `npm install` at the root of the folder to install the node dependencies.
+- Requires an Opensea API key. To add your key, copy the file `.env.template` to just `.env` and replace the "12345" with your API key.
 
 ## Running
 
-- Run the script directly using `npm run start --slug=doodles-official` in the terminal at the root of the folder.
+- Run the script directly using for example `npm run start --slug=doodles-official` in the terminal at the root of the folder.
 
 #### Parameters
 
 `--slug` The Opensea slug of the collection. This is required if contract is not specified.
 `--contract` The contract address of the collection. This is required if slug is not specified.
 `--outputFileName` The name of the CSV file to output. Optional.
-`--since` Unix timestamp to filter results. Will only query transactions after this timestamp. Optional.
+`--daysback` Number of days worth of data until the current date to pull back from Opensea. Optional.
--- index.js
+++ index.js
@@ -4,10 +4,25 @@ import fetch from "node-fetch";
 
 config();
 
-export const sleep = (ms) =>
+const sleep = (ms) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+
+const getHMS = (ms) => {
+  let seconds = ms / 1000;
+  // 2- Extract hours:
+  const hours = parseInt(seconds / 3600); // 3,600 seconds in 1 hour
+  seconds = seconds % 3600; // seconds remaining after extracting hours
+  // 3- Extract minutes:
+  const minutes = parseInt(seconds / 60); // 60 seconds in 1 minute
+  // 4- Keep only seconds not extracted to minutes:
+  seconds = parseInt(seconds % 60);
+
+  return { hours, minutes, seconds };
+};
+
+const SPACES = " ".repeat(40);
 
 const THROTTLED_REGEX =
   /Request was throttled\.( Expected available in ([0-9]*) second)?/;
@@ -15,9 +30,10 @@ const THROTTLED_REGEX =
 const OPENSEA_URL =
   "https://api.opensea.io/api/v1/events?only_opensea=false&event_type=successful";
 
-const MINUTES = 0.5;
+const MINUTES = 1 / 120;
 const LOG_INTERVAL = 60000 * MINUTES;
 const ONE_DAY = 86400000;
+const DOTS = "▏▎▍▋▊▉";
 
 let cachedHeaders;
 
@@ -49,25 +65,10 @@ const go = async () => {
 
   const since = Math.floor((now.getTime() - days * ONE_DAY) / 1000);
 
-  const dt =
-    now.toLocaleDateString().replace(/\//g, "-") +
-    "_" +
-    now
-      .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-      .replace(":", "-")
-      .replace(" ", "");
-
   if (slug) {
     requestUrl += `&collection_slug=${slug}`;
-    if (!filename) {
-      filename = `${slug}_${dt}.csv`;
-    }
   } else if (contract) {
     requestUrl += `&asset_contract_address=${contract}`;
-
-    if (!filename) {
-      filename = `${contract}_${dt}.csv`;
-    }
   } else {
     console.error(
       "This script requires either a `--slug` or `--contract` parameter to be passed."
@@ -79,22 +80,31 @@ const go = async () => {
     requestUrl += `&occurred_after=${since}`;
   }
 
-  if (!filename?.endsWith(".csv")) filename += ".csv";
-
-  let cont = true;
-  let i = 0;
-  let j = 0;
-  let k = 0;
-  let cursor = "";
-  let failedAttempts = 0;
   const result = {};
+  const startTime = Date.now();
+
+  let loop = true;
+  let cursor = "";
+  let txCount = 0;
+  let reqCount = 0;
+  let intervalCount = 0;
+  let failedAttempts = 0;
 
   setInterval(() => {
-    const minutesDuration = ++k * MINUTES;
-    console.info(
-      `${minutesDuration} minutes passed. ${i} tx processed (~${Math.round(
-        i / minutesDuration
-      )} / min) in ${j} requests (~${Math.round(j / minutesDuration)} / min).`
+    const duration = Date.now() - startTime;
+    const { minutes, seconds } = getHMS(duration);
+    const fullMins = duration / 1000 / 60;
+
+    process.stdout.write(
+      `${`${DOTS.charAt(++intervalCount % DOTS.length)} ${"0".repeat(
+        Math.max(2 - String(minutes).length, 0)
+      )}${minutes}`}:${`${"0".repeat(
+        Math.max(2 - String(seconds).length, 0)
+      )}${seconds}`} - ${txCount} tx processed (~${Math.round(
+        txCount / fullMins
+      )}/min) in ${reqCount} requests (~${Math.round(
+        reqCount / fullMins
+      )}/min)${SPACES}\r`
     );
   }, LOG_INTERVAL);
   let collectionName;
@@ -112,14 +122,16 @@ const go = async () => {
       const match = THROTTLED_REGEX.exec(detail);
       if (match) {
         const [_, secs] = match;
-        const msToWait = (parseInt(secs) ?? 10) * 1010;
-        console.info(`Opensea API rate limited. Retrying in ${secs} seconds.`);
+        const msToWait = (parseInt(secs) || 10) * 1010;
+        console.info(
+          `Opensea API rate limited. Retrying in ${secs} seconds.${SPACES}`
+        );
         await sleep(msToWait);
       } else {
-        if (j === 0 && asset_events.length) {
-          collectionName = asset_events[0].asset_contract.name;
+        if (reqCount === 0 && asset_events.length) {
+          collectionName = asset_events[0].asset.asset_contract.name;
         }
-        j++;
+        reqCount++;
         cursor = next;
         asset_events.forEach(
           ({
@@ -132,11 +144,11 @@ const go = async () => {
 
             if (!result[date]) result[date] = [priceEth];
             else result[date].push(priceEth);
-            i++;
+            txCount++;
           }
         );
         if (!cursor) {
-          cont = false;
+          loop = false;
         } else {
           failedAttempts = 0;
         }
@@ -144,27 +156,31 @@ const go = async () => {
     } catch (e) {
       failedAttempts++;
       if (failedAttempts >= 5) {
-        console.error("An unexpected error occurred requesting Opensea data.");
+        console.error(
+          `An unexpected error occurred requesting Opensea data.${SPACES}`
+        );
         console.error(e.message);
         process.exit(1);
       } else {
         const wait = 1000 * failedAttempts;
         await sleep(wait);
-        console.error(
-          `Error fetching Opensea data x${failedAttempts}. Retrying in ${
+        process.stdout.write(
+          ` Error fetching Opensea data x${failedAttempts}. Retrying in ${
             wait / 1000
-          } seconds.`
+          } seconds.${SPACES}\r`
         );
       }
     }
-  } while (cont);
+  } while (loop);
 
+  let rows = 0;
   const file = Object.entries(result).reduce((acc, [date, prices]) => {
     const volume = prices.reduce((sum, val) => sum + val, 0);
     const avgPrice =
       Math.round((volume / prices.length + Number.EPSILON) * 100) / 100;
     const floor = Math.min(...prices);
 
+    rows++;
     return acc + `${date},${volume},${avgPrice},${floor},${prices.length}\n`;
   }, 'Date,Volume,"Avg Price",Floor,"Num Sales"\n');
 
@@ -172,8 +188,31 @@ const go = async () => {
     await promises.mkdir("./output");
   }
 
+  const dt =
+    now.toLocaleDateString().replace(/\//g, "-") +
+    "_" +
+    now
+      .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+      .replace(":", "-")
+      .replace(" ", "");
+
+  if (!filename) {
+    filename = `${collectionName
+      .toLocaleLowerCase()
+      .replace(/ /g, "-")}_${dt}.csv`;
+  }
+  if (!filename?.endsWith(".csv")) filename += ".csv";
+
   await promises.writeFile(`output/${filename}`, file);
-  console.info(`Written to output/${filename}`);
+
+  const duration = Date.now() - startTime;
+  let seconds = duration / 1000;
+  const minutes = parseInt(seconds / 60);
+  seconds = Math.round(seconds % 60);
+
+  console.info(
+    `Completed in ${minutes}m ${seconds}s. ${rows} data rows written to output/${filename}${SPACES}`
+  );
   process.exit(0);
 };
 
